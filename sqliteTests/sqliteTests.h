fa70fbc741e32639d33286719af8efe8e152e727
Lenny <lenny@hothouselabs.com>
2013-07-21 21:20:29 -0700

    first commit

---
+++ sqliteTests/sqliteTests.h
@@ -0,0 +1,13 @@
+//
+//  sqliteTests.h
+//  sqliteTests
+//
+//  Created by Lenny Turetsky on 07/21/13.
+//  Copyright (c) 2013 Lenny Turetsky. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface sqliteTests : SenTestCase
+
+@end
---
+++ sqliteTests/sqliteTests.m
@@ -0,0 +1,32 @@
+//
+//  sqliteTests.m
+//  sqliteTests
+//
+//  Created by Lenny Turetsky on 07/21/13.
+//  Copyright (c) 2013 Lenny Turetsky. All rights reserved.
+//
+
+#import "sqliteTests.h"
+
+@implementation sqliteTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in sqliteTests");
+}
+
+@end
